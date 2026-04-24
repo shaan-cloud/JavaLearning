5bc4bda272734ad35933c7800ff3be65dc096cbf
shaan <shaan.v@mlzschennai.com>
2026-04-24 10:50:20 +0530

    Day 1 complete - variables and conditions

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,44 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        String playerName = "Steve";
+        int playerHealth = 9;
+        int hungerLevel = 6;
+        boolean isOnFire = true;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("===" + playerName + "'s STATUS ===");
+
+        if (playerHealth <= 0) {
+            System.out.println(playerName + " has died!... Respawn??");
+        } else if (playerHealth <= 5) {
+            System.out.println("Hey Careful! you are at Critical Health!");
+        } else if (playerHealth <= 10) {
+            System.out.println("Health is getting Low! gather some food immediately!");
+        } else {
+            System.out.println("Your Good!!");
         }
+
+        if (hungerLevel <= 0) {
+            System.out.println("Hey you are starving! Quick! Eat!");
+        } else if (hungerLevel <= 3) {
+            System.out.println("Very hungry! Eat something!");
+        } else {
+            System.out.println("Hunger is fine! Good to go!");
+        }
+
+        if (isOnFire == true) {
+            System.out.println("Hey you're on Fire!! Quick! go to a water source!");
+
+        }
+
+        if(playerHealth>6 && hungerLevel > 3){
+            System.out.println("You can sprint!!!");
+
+        }
+        else {
+            System.out.println("You are too weak to sprint. Please Increase your hungerLevel");
+        }
+
+
     }
+
 }
